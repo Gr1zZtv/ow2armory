3551ce59b3ed67b45e46f7f0e714ebf434b14e46
Gr1zZtv <104893867+Gr1zZtv@users.noreply.github.com>
2025-04-26 17:29:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const path    = require('path');
 const app     = express();
+const { expressjwt: jwt } = require('express-jwt');
+const jwksRsa            = require('jwks-rsa');
+
+// 1) Middleware to validate the access token
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksUri: `dev-bx2itlkpy8ayes2m.us.auth0.com/.well-known/jwks.json`
+  }),
+  audience: 'https://dev-bx2itlkpy8ayes2m.us.auth0.com/api/v2/',   // create an API in Auth0 to get this, or use your Client ID
+  issuer:   `dev-bx2itlkpy8ayes2m.us.auth0.com`,
+  algorithms: ['RS256']
+});
 
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
